0b2cf1d5f06129a6d43b41126dfc109e05181442
eduardo <eduardo@DESKTOP-6KT589L>
2025-11-02 21:57:19 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiAppHotel")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiAppHotel.Pages")]
